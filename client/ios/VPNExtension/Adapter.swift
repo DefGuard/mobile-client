e40a9fa8ff1a041784097e282f1d6a3bcf8d9900
Adam Ciarciński <aciarcinski@teonite.com>
2025-07-21 14:13:59 +0200

    iOS: handle network path changes

--- client/ios/VPNExtension/Adapter.swift
+++ client/ios/VPNExtension/Adapter.swift
@@ -8,12 +8,16 @@ final class Adapter /*: Sendable*/ {
     private weak var packetTunnelProvider: NEPacketTunnelProvider?
     /// BortingTun tunnel
     private var tunnel: Tunnel?
+    /// UDP endpoint
+    private var endpoint: Network.NWEndpoint?
     /// Server connection
     private var connection: NWConnection?
+    /// Network routes monitor.
+    private var networkMonitor: NWPathMonitor?
     /// Keep alive timer
     private var keepAliveTimer: Timer?
     /// Logging
-    private var logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "Adapter")
+    private lazy var logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "Adapter")
 
     /// Designated initializer.
     /// - Parameter packetTunnelProvider: an instance of `NEPacketTunnelProvider`. Internally stored
@@ -28,9 +32,16 @@ final class Adapter /*: Sendable*/ {
     public func start(tunnelConfiguration: TunnelConfiguration) throws {
         if let _ = tunnel {
             logger.info("Cleaning exiting Tunnel...")
-            self.tunnel = nil
-            self.connection = nil
+            tunnel = nil
+            connection = nil
         }
+
+        let networkMonitor = NWPathMonitor()
+        networkMonitor.pathUpdateHandler = { [weak self] path in
+            self?.networkPathUpdate(path: path)
+        }
+        networkMonitor.start(queue: .main)
+        self.networkMonitor = networkMonitor
 
         logger.info("Initializing Tunnel...")
         tunnel = try Tunnel.init(
@@ -46,30 +57,8 @@ final class Adapter /*: Sendable*/ {
             logger.error("Endpoint is nil")
             return
         }
-        let params = NWParameters.udp
-        params.allowLocalEndpointReuse = true
-        connection = NWConnection.init(to: endpoint.asNWEndpoint(), using: params)
-
-        //connection?.stateUpdateHandler = { state in
-        //    print("UDP connection state: \(state)")
-        //}
-        logger.info("Receiving UDP from endpoint...")
-        connection?.start(queue: .main)
-        // Send initial handshake packet
-        if let tunnel = tunnel {
-            handleTunnelResult(tunnel.forceHandshake())
-        }
-        receive()
-
-        // Use Timer to send keep-alive packets.
-        keepAliveTimer?.invalidate()
-        logger.info("Creating keep-alive timer...")
-        let timer = Timer(timeInterval: 0.25, repeats: true) { timer in
-            guard let tunnel = self.tunnel else { return }
-            self.handleTunnelResult(tunnel.tick())
-        }
-        keepAliveTimer = timer
-        RunLoop.main.add(timer, forMode: .common)
+        self.endpoint = endpoint.asNWEndpoint()
+        initEndpoint()
 
         logger.info("Sniffing packets...")
         readPackets()
@@ -82,6 +71,9 @@ final class Adapter /*: Sendable*/ {
         tunnel = nil
         keepAliveTimer?.invalidate()
         keepAliveTimer = nil
+        // Cancel network monitor
+        networkMonitor?.cancel()
+        networkMonitor = nil
         logger.info("Tunnel stopped")
     }
 
@@ -92,7 +84,7 @@ final class Adapter /*: Sendable*/ {
             case .err(let error):
                 logger.error("Tunnel error \(String(describing: error))")
                 if error == WireGuardError.connectionExpired {
-                    logger.error("Connecion has expiered - stopping the tunnel")
+                    logger.error("Connecion has expired - stopping the tunnel")
                     stop()
                 }
                 break
@@ -107,28 +99,78 @@ final class Adapter /*: Sendable*/ {
         }
     }
 
-    func sendToEndpoint(data: Data) {
-        connection?.send(content: data, completion: .contentProcessed { error in
-            if let error = error {
-                self.logger.error("Send error: \(error)")
-            }
-        })
+    /// Initialise UDP connection to endpoint.
+    private func initEndpoint() {
+        guard let endpoint = endpoint else { return }
+
+        logger.info("Init Endpoint")
+        // Cancel previous connection
+        connection?.cancel()
+        connection = nil
+
+        let params = NWParameters.udp
+        params.allowLocalEndpointReuse = true
+        let connection = NWConnection.init(to: endpoint, using: params)
+        connection.stateUpdateHandler = { [weak self] state in
+            self?.endpointStateChange(state: state)
+        }
+
+        connection.start(queue: .main)
+        self.connection = connection
+    }
+
+    /// Setup UDP connection to endpoint. This method should called when UDP connection is ready to send and receive.
+    private func setupEndpoint() {
+        logger.info("Setup Endpoint")
+
+        // Send initial handshake packet
+        if let tunnel = self.tunnel {
+            handleTunnelResult(tunnel.forceHandshake())
+        }
+        logger.info("Receiving UDP from endpoint...")
+        receive()
+
+        // Use Timer to send keep-alive packets.
+        keepAliveTimer?.invalidate()
+        logger.info("Creating keep-alive timer...")
+        let timer = Timer(timeInterval: 0.25, repeats: true) { [weak self] timer in
+            guard let self = self, let tunnel = self.tunnel else { return }
+            self.handleTunnelResult(tunnel.tick())
+        }
+        keepAliveTimer = timer
+        RunLoop.main.add(timer, forMode: .common)
+    }
+
+    /// Send packets to UDP endpoint.
+    private func sendToEndpoint(data: Data) {
+        guard let connection = connection else { return }
+        if connection.state == .ready {
+            connection.send(content: data, completion: .contentProcessed { error in
+                if let error = error {
+                    self.logger.error("UDP connection send error: \(error, privacy: .public)")
+                }
+            })
+        } else {
+            logger.warning("UDP connection not ready to send")
+        }
     }
 
     /// Handle UDP packets from the endpoint.
     private func receive() {
-        guard let connection = connection else { return }
-        connection.receiveMessage { data, context, isComplete, error in
+        connection?.receiveMessage { [weak self] data, context, isComplete, error in
+            guard let self = self else { return }
             if let data = data, let tunnel = self.tunnel {
                 self.handleTunnelResult(tunnel.read(src: data))
             }
             if error == nil {
-                self.receive() // continue receiving
+                // continue receiving
+                self.receive()
             }
         }
     }
 
-    func readPackets() {
+    /// Read tunnel packets.
+    private func readPackets() {
         guard let tunnel = self.tunnel else { return }
 
         // Packets received to the tunnel's virtual interface.
@@ -136,7 +178,31 @@ final class Adapter /*: Sendable*/ {
             for packet in packets  {
                 self.handleTunnelResult(tunnel.write(src: packet.data))
             }
+            // continue reading
             self.readPackets()
+        }
+    }
+
+    /// Handle UDP connection state changes.
+    private func endpointStateChange(state: NWConnection.State) {
+        logger.debug("UDP connection state: \(String(describing: state), privacy: .public)")
+        switch state {
+            case .ready:
+                setupEndpoint()
+            default:
+                break
+        }
+    }
+
+    /// Handle network path updates.
+    private func networkPathUpdate(path: Network.NWPath) {
+        if path.status == .unsatisfied {
+            logger.warning("Unsatisfied network path: going dormant")
+            connection?.cancel()
+            connection = nil
+        } else {
+            logger.warning("Satisfied network path: going running")
+            initEndpoint()
         }
     }
 }
