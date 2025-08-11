ba45948e073f3ba7b7d469711cf60a85e7356813
Filip Ślęzak <fslezak@teonite.com>
2025-08-11 09:59:34 +0200

    fix connection state drop

--- client/lib/open/riverpod/plugin/plugin.g.dart
+++ client/lib/open/riverpod/plugin/plugin.g.dart
@@ -7,15 +7,12 @@ part of 'plugin.dart';
 // **************************************************************************
 
 String _$pluginActiveTunnelStateHash() =>
-    r'b9062b130998fa201c60ffaab1ab66c8e146e2ff';
+    r'55599a4824466a2d18b3333ee242975b0de1c68b';
 
 /// See also [PluginActiveTunnelState].
 @ProviderFor(PluginActiveTunnelState)
 final pluginActiveTunnelStateProvider =
-    AutoDisposeNotifierProvider<
-      PluginActiveTunnelState,
-      PluginTunnelEventData?
-    >.internal(
+    NotifierProvider<PluginActiveTunnelState, PluginTunnelEventData?>.internal(
       PluginActiveTunnelState.new,
       name: r'pluginActiveTunnelStateProvider',
       debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -25,6 +22,6 @@ final pluginActiveTunnelStateProvider =
       allTransitiveDependencies: null,
     );
 
-typedef _$PluginActiveTunnelState = AutoDisposeNotifier<PluginTunnelEventData?>;
+typedef _$PluginActiveTunnelState = Notifier<PluginTunnelEventData?>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
