c1ed5b826759271cb85d04f3363d85a9e40ec0c6
Jacek Chmielewski <jacek@defguard.net>
2026-05-25 11:46:30 +0200

    update deps, fix ndk version mismatch

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
           allowUnfree = true;
         };
       };
-      ndkVersion = "27.0.12077973";
+      ndkVersion = "28.2.13676358";
       androidComposition = pkgs.androidenv.composeAndroidPackages {
         # buildToolsVersions = [ buildToolsVersion "28.0.3" ];
         # platformVersions = [ "34" "28" ];
